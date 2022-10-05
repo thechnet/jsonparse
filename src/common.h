b0ce49105ad104a5414c31303caac8dbab5aed89
chnet <thechnet@gmail.com>
2022-10-05 11:49:19 +0200

    Move options from common.h to CMakeLists

--- src/common.h
+++ src/common.h
@@ -1,14 +1,15 @@
 /*
 common.h - jsonparse
-Modified 2022-09-18
+Modified 2022-10-05
 */
 
 #ifndef JSON_COMMON_H
 #define JSON_COMMON_H
 
 /* Debugging. */
-// #define WATCHDOG
-// #define JSON_PARSER_PRINT_PARSED_CHARACTERS
+#if !defined(NDEBUG) && defined(JSON_DEBUG_ENABLE_WATCHDOG)
+#define WATCHDOG
+#endif
 
 /* Header-specific includes. */
 #include <stdbool.h>
